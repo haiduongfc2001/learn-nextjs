0dd91fd3adc796fb11dce8ca109fe80b90004885
haiduongfc2001 <haiduongfc2001@gmail.com>
2024-01-29 23:52:52 +0700

    nextjs: Automatic Static Optimization

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,7 +1,18 @@
+import { useRouter } from 'next/dist/client/router'
 import React from 'react'
 
 export interface AboutPageProps {}
 
 export default function AboutPage(props: AboutPageProps) {
-  return <div>About Page</div>
+	const router = useRouter()
+
+	console.log('About query: ', router.query)
+
+	return <div>About Page</div>
+}
+
+export async function getServerSideProps() {
+	return {
+		props: {}, // will be passed to the page component as props
+	}
 }
